cd90c96c9911c79b5d636c930c657f9f9f09df2f
luojiajie111 <76584231+luojiajie111@users.noreply.github.com>
2022-12-29 23:08:28 +0900

    first sumbit

--- src/components/Display/index.jsx
+++ src/components/Display/index.jsx
@@ -20,17 +20,17 @@ export default class index extends Component {
       city: Tdata.result,
     })
     //データinit
-    const Hcount = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=13`,{
+    const peopleNumber = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=13`,{
       method:'GET',
       headers:{
         'Content-Type': 'application/json',
         'X-API-KEY':'fJIvBBcHj8cjwd4KdSRVsCSlHzz1z8jbap7HF52R'
       }
     })
-    const Hnumber = await Hcount.json();
-    const totalNumber = Hnumber.result.data[0].data;
+    const people = await peopleNumber.json();
+    const totalPeopleNumber = people.result.data[0].data;
     const tempArr = []; 
-    totalNumber.forEach((item,index) => {
+    totalPeopleNumber.forEach((item,index) => {
         tempArr.push(item.value);
     });
     var numb = this.state.number;
@@ -47,20 +47,20 @@ export default class index extends Component {
   handleNumber=(prefName, prefCode)=>{
     return async (e)=>{
       if(e.target.checked){
-        const Hcount = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,{
+        const peopleNumber = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${prefCode}`,{
           method:'GET',
           headers:{
             'Content-Type': 'application/json',
             'X-API-KEY':'fJIvBBcHj8cjwd4KdSRVsCSlHzz1z8jbap7HF52R'
           }
         })
-        const Hnumber = await Hcount.json();
-        const totalNumber = Hnumber.result.data[0].data;
+        const people = await peopleNumber.json();
+        const totalPeople = people.result.data[0].data;
         const tempArr = [];
-        totalNumber.forEach((item,index) => {
+        totalPeople.forEach((item,index) => {
             tempArr.push(item.value);
         });
-        var numb = this.state.number;
+        let numb = this.state.number;
         numb.push({
           name: prefName,
           data:tempArr
@@ -69,12 +69,13 @@ export default class index extends Component {
           number: numb
         })
       } else {
-        //データdeleteS
-        var numb = this.state.number;
+        //データdelete
+        let numb = this.state.number;
         numb.map((item, index)=>{
-          if(item.name == prefName){
+          if(item.name === prefName){
             numb.splice(index, 1);
           }
+          return item.id
         })
         this.setState({
           number:numb
@@ -83,7 +84,7 @@ export default class index extends Component {
     }
   }
   componentDidUpdate(){
-    const chart = Highcharts.chart('graph', {
+    Highcharts.chart('graph', {
       title: {
         text: '都道府県総人数',
       },
@@ -91,6 +92,9 @@ export default class index extends Component {
         title:{
           text:'年度'
         },
+      },
+      accessibility: {
+        enabled: false
       },
       yAxis: {
         title: {
@@ -140,7 +144,7 @@ export default class index extends Component {
           return(
             <label className='label_item' key={index}>
               <span className='wrap_item'>
-                <input className="wrap_input" defaultChecked={item.prefCode == 13? true:false} type="checkbox" onChange={this.handleNumber(item.prefName,item.prefCode)}/>{item.prefName}
+                <input className="wrap_input" defaultChecked={item.prefCode === 13? true:false} type="checkbox" onChange={this.handleNumber(item.prefName,item.prefCode)}/>{item.prefName}
               </span>
             </label>
           )
